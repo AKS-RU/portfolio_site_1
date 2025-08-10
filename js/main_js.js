838d6e81cbc71af1bc0d974ce938e2103116ae5a
AKS-RU <almetman.aks@gmail.com>
2025-08-11 01:56:32 +0300

    Сделаны блоки video и projects

--- js/main_js.js
+++ js/main_js.js
@@ -1,1 +1,4 @@
-// #
+  document.addEventListener('DOMContentLoaded', function() {
+    var image = document.querySelectorAll('.materialboxed');
+    var instances_image = M.Materialbox.init(image);
+  });
